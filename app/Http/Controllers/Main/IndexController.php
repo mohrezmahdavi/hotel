25b625df8e6dc93c4bb6fbf0628edf6362b3a372
Mohammadreza Mahdavi <mrmahdavi38@gmail.com>
2022-12-21 13:11:13 +0330

    cities list cached

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -5,13 +5,22 @@ namespace App\Http\Controllers\Main;
 use App\Http\Controllers\Controller;
 use App\Models\Hotel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class IndexController extends Controller
 {
     public function index(Request $request)
     {
-        $cites = Hotel::select('PlaceID','PlaceName')->groupBy('PlaceID')
-        ->get()->toArray();
+        $cites = $this->getHotels();
         return view('main.index.index', compact('cites'));
+    }
+
+    private function getHotels()
+    {
+        return Cache::rememberForever('cities', function () {
+            return Hotel::select('PlaceID','PlaceName')->groupBy('PlaceID')
+            ->get()->toArray();
+        });
+        
     }
 }
